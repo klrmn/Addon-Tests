8b7fbfd3c752ee47fd8f699a9ec815adebe72169
Leah Klearman <lklrmn@gmail.com>
2012-06-20 15:58:46 -0700

    none of the personas tests actually use NativeEvents

--- tests/desktop/test_personas.py
+++ tests/desktop/test_personas.py
@@ -16,7 +16,6 @@ from pages.desktop.personas import Personas
 
 class TestPersonas:
 
-    @pytest.mark.native
     @pytest.mark.nondestructive
     def test_start_exploring_link_in_the_promo_box(self, mozwebqa):
         """
@@ -32,7 +31,6 @@ class TestPersonas:
         Assert.equal("up-and-coming", browse_personas_page.sort_key)
         Assert.equal("Up & Coming", browse_personas_page.sort_by)
 
-    @pytest.mark.native
     @pytest.mark.nondestructive
     def test_page_title_for_personas_landing_page(self, mozwebqa):
         """
@@ -43,7 +41,6 @@ class TestPersonas:
         personas_page = home_page.header.site_navigation_menu("Personas").click()
         Assert.true(personas_page.is_the_current_page)
 
-    @pytest.mark.native
     @pytest.mark.smoke
     @pytest.mark.nondestructive
     def test_the_featured_personas_section(self, mozwebqa):
@@ -56,7 +53,6 @@ class TestPersonas:
         Assert.true(personas_page.is_the_current_page)
         Assert.less_equal(personas_page.featured_personas_count, 6)
 
-    @pytest.mark.native
     @pytest.mark.smoke
     @pytest.mark.nondestructive
     def test_the_recently_added_section(self, mozwebqa):
@@ -71,7 +67,6 @@ class TestPersonas:
         recently_added_dates = personas_page.recently_added_dates
         Assert.is_sorted_descending(recently_added_dates)
 
-    @pytest.mark.native
     @pytest.mark.smoke
     @pytest.mark.nondestructive
     def test_the_most_popular_section(self, mozwebqa):
@@ -86,7 +81,6 @@ class TestPersonas:
         downloads = personas_page.most_popular_downloads
         Assert.is_sorted_descending(downloads)
 
-    @pytest.mark.native
     @pytest.mark.nondestructive
     def test_the_top_rated_section(self, mozwebqa):
         """
@@ -100,7 +94,6 @@ class TestPersonas:
         ratings = personas_page.top_rated_ratings
         Assert.is_sorted_descending(ratings)
 
-    @pytest.mark.native
     @pytest.mark.nondestructive
     def test_breadcrumb_menu_in_persona_details_page(self, mozwebqa):
         """
@@ -140,7 +133,6 @@ class TestPersonas:
         personas_detail_page.breadcrumbs[0].click()
         Assert.true(home_page.is_the_current_page)
 
-    @pytest.mark.native
     @pytest.mark.nondestructive
     def test_personas_breadcrumb_format(self, mozwebqa):
         """
